75e7f8d60c0baa44ad37cc1c34dce071e0ecdcbf
Quân Nguyễn <92315466+QuanNguyen0804@users.noreply.github.com>
2022-10-22 17:00:18 +0700

    Initial commit

---
+++ CalculatorAPI/Controllers/CurrencyConverterController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CurrencyConverterAPI.Controllers
+{
+   
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CurrencyConverterController : ControllerBase
+    {
+        // GET: api/CurrencyConverter
+        [HttpGet("convert/{a}/{b}")]
+        public string add(float a, float b)
+        {
+            CurrencyConverterAPI.Model.CurrencyConverter cal = new Model.CurrencyConverter(a, b);
+            cal.mul();
+            return cal.result.ToString();
+        }
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Currency_ConverterAPI
+ Project of "Phát triển ứng dụng HTTT hiện đại", use .Net API
